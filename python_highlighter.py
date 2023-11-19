0d4b081ff3cb6dfbe5fda121098b49d482dba4c8
artur <arturkummer08@gmail.com>
2023-11-19 10:55:57 +0100

    self

--- python_highlighter.py
+++ python_highlighter.py
@@ -14,7 +14,7 @@ class PythonHighlighter(QSyntaxHighlighter):
             'False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break',
             'class', 'continue', 'del', 'def', 'elif', 'else', 'except', 'finally', 'for',
             'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not',
-            'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield'
+            'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield', 'self'
         ]
 
         self.highlightingRules = [(QRegularExpression(r'\b' + keyword + r'\b'), keywordFormat) for keyword in keywords]
